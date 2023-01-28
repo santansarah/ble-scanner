bcfeebb93a631bf492005a21aec6451327e4c1bf
sarah <santansarah@gmail.com>
2023-01-28 15:50:41 -0700

    Compose previews.

--- app/src/main/java/com/santansarah/blescanner/presentation/scan/Home.kt
+++ app/src/main/java/com/santansarah/blescanner/presentation/scan/Home.kt
@@ -1,28 +1,22 @@
 package com.santansarah.blescanner.presentation.scan
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.santansarah.blescanner.data.local.entities.ScannedDevice
 import com.santansarah.blescanner.presentation.components.ShowPermissions
+import com.santansarah.blescanner.presentation.theme.BLEScannerTheme
 import com.santansarah.blescanner.utils.permissionsList
 import org.koin.androidx.compose.koinViewModel
 
@@ -32,7 +26,7 @@ fun HomeRoute(
     vm: ScanViewModel = koinViewModel()
 ) {
 
-    val devices = vm.deviceList.collectAsStateWithLifecycle()
+    val devices = vm.deviceList.collectAsStateWithLifecycle().value
     val multiplePermissionsState = rememberMultiplePermissionsState(permissions = permissionsList)
 
     LaunchedEffect(key1 = multiplePermissionsState.allPermissionsGranted) {
@@ -44,23 +38,62 @@ fun HomeRoute(
     if (!multiplePermissionsState.allPermissionsGranted) {
         ShowPermissions(multiplePermissionsState)
     } else {
-
-        LazyColumn() {
-            items(devices.value) { device ->
-
-
-                ScannedDevice(device = device, onClick = vm::connectToDevice)
-
-                Spacer(modifier = Modifier.height(10.dp))
-
-            }
-        }
+        ScannedDeviceList(devices, vm::connectToDevice)
     }
 
 }
 
-@Preview
+@Composable
+private fun ScannedDeviceList(
+    devices: List<ScannedDevice>,
+    onClick: (String) -> Unit
+) {
+    LazyColumn() {
+        items(devices) { device ->
+
+            ScannedDevice(device = device, onClick = onClick)
+            Spacer(modifier = Modifier.height(10.dp))
+
+        }
+    }
+}
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
 @Composable
 fun ListPreview() {
+
+    val deviceList = listOf(
+        ScannedDevice(
+            deviceId = 0,
+            deviceName = "ELK-BLEDOM",
+            address = "BE:00:FA:00:XX:00",
+            rssi = -77,
+            manufacturer = null,
+            services = listOf("[Human Interface Device"),
+            extra = null,
+            lastSeen = 1674510398719
+        ),
+        ScannedDevice(
+            deviceId = 0,
+            deviceName = "EASYWAY-BLE",
+            address = "93:00:44:00:XX:AC",
+            rssi = -81,
+            manufacturer = "Ericsson Technology Licensing",
+            services = null,
+            extra = null,
+            lastSeen = 1674510397416
+        )
+    )
+
+    BLEScannerTheme {
+        Surface() {
+            ScannedDeviceList(devices = deviceList, onClick = {})
+        }
+    }
 
 }
---
+++ sharedTest/src/main/java/com/santansarah/sharedtest/previews/ScannedDevice.kt
@@ -0,0 +1,41 @@
+package com.santansarah.sharedtest.previews
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.santansarah.blescanner.data.local.entities.ScannedDevice
+import com.santansarah.blescanner.presentation.scan.ScannedDevice
+import com.santansarah.blescanner.presentation.theme.BLEScannerTheme
+import com.santansarah.sharedtest.R
+import com.santansarah.sharedtest.newDevice
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+@Composable
+fun ScannedDevicePreview() {
+    val device = newDevice
+    BLEScannerTheme {
+        Surface() {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+
+                ScannedDevice(device = device, onClick = {})
+
+            }
+        }
+    }
+}
