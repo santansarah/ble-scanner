5d22192e58c90821e9758f10f4a0694265d44412
sarah <santansarah@gmail.com>
2023-03-05 15:35:57 -0700

    End to end home screen JUnit 5 compose tests.

--- app/src/androidTest/java/com/santansarah/blescanner/presentation/scan/ScanListFilters.kt
+++ app/src/androidTest/java/com/santansarah/blescanner/presentation/scan/ScanListFilters.kt
@@ -3,8 +3,13 @@ package com.santansarah.blescanner.presentation.scan
 import android.app.Application
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.le.BluetoothLeScanner
+import androidx.compose.ui.graphics.asAndroidBitmap
 import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.captureToImage
 import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
 import com.santansarah.blescanner.di.endtoend.endToEndModule
 import com.santansarah.blescanner.domain.interfaces.IBleRepository
 import com.santansarah.blescanner.domain.models.ConnectionState
@@ -12,7 +17,7 @@ import com.santansarah.blescanner.domain.models.ScanFilterOption
 import com.santansarah.blescanner.presentation.BleGatt
 import com.santansarah.blescanner.presentation.BleManager
 import com.santansarah.blescanner.presentation.MainActivity
-import com.santansarah.blescanner.util.waitUntilTimeout
+import com.santansarah.blescanner.util.saveScreenshot
 import com.santansarah.sharedtest.deviceDetail
 import com.santansarah.sharedtest.deviceList
 import de.mannodermaus.junit5.compose.createAndroidComposeExtension
@@ -21,6 +26,7 @@ import io.mockk.every
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
@@ -86,10 +92,36 @@ class ScanListFilters : KoinTest {
         every { bleRepository.getScannedDevices(null) } returns flow
 
         extension.runComposeTest {
-            waitUntilTimeout(2000)
 
             onAllNodes(hasText("Last seen:", substring = true)).assertCountEquals(3)
+            val bitmap = onRoot().captureToImage().asAndroidBitmap()
+            saveScreenshot(
+                "homeScanListUnfiltered"
+                        + System.currentTimeMillis().toString(), bitmap
+            )
+        }
 
+    }
+
+    @Test
+    fun scanListFavorites() {
+
+        every { bleRepository.getScannedDevices(null) } returns flow.onEach {
+            it.filter { device -> device.favorite }
+        }
+
+        extension.runComposeTest {
+
+            onNodeWithText("Favorites").performClick()
+
+            onAllNodes(hasText("Last seen:", substring = true)).assertCountEquals(1)
+            onNodeWithText("GOOD-RSSI").assertExists()
+
+            val bitmap = onRoot().captureToImage().asAndroidBitmap()
+            saveScreenshot(
+                "homeScanListFavorites"
+                        + System.currentTimeMillis().toString(), bitmap
+            )
         }
 
     }
